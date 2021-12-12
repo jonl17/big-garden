09e4bf0bc355a1d29e4340ae341aa7d2d421bd1a
Jón Gabríel Lorange <jonl17@ru.is>
2021-12-12 12:52:46 +0000

    control pnale

--- src/components/ControlPanel/ControlPanel.tsx
+++ src/components/ControlPanel/ControlPanel.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const ControlPanel = ({ position }: Props) => {
   return (
-    <div className='absolute bottom-5 left-5'>
+    <div className='fixed top-0 right-0'>
       <p>lat: {position.latitude}</p>
       <p>lng: {position.longitude}</p>
     </div>
